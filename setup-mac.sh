97506613d4c498a42fc4cc5d83c995a9b328d5e6
acopar <andrej.copar@gmail.com>
2015-02-16 20:02:39 +0100

    Update Mac install script.

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap homebrew/science
-brew install git python3 gcc suite-sparse swig
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"  && 
+brew tap homebrew/science &&
+brew install makedepend git python3 gcc suite-sparse swig &&
 
-pip3 install virtualenv
-virtualenv -p python3 orange3env
-source orange3env/bin/activate
+pip3 install virtualenv &&
+virtualenv -p python3 orange3env &&
+source orange3env/bin/activate &&
 
-git clone https://github.com/biolab/orange3
-cd orange3
-pip install -r requirements.txt
-python setup.py develop
-pip install ipython "ipython[notebook]" Orange-Bioinformatics networkx python-igraph
+git clone https://github.com/biolab/orange3 &&
+cd orange3 &&
+pip install numpy &&
+pip install -r requirements.txt &&
+python setup.py develop &&
+pip install ipython "ipython[notebook]" Orange-Bioinformatics networkx python-igraph &&
 
 ipython notebook

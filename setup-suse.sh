a82a97bd94135f2573c7cd74f06bc1a2c4192f76
acopar <andrej.copar@gmail.com>
2015-02-16 18:25:54 +0100

    Update Mac install script.

--- setup-suse.sh
+++ setup-suse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo zypper refresh
-sudo zypper install git python-virtualenv python3-devel gcc gcc-c++ python3-numpy python3-numpy-devel python3-scipy python3-matplotlib python3-IPython libxml2-devel lzlib-devel 
+sudo zypper install git python-virtualenv python3-devel gcc gcc-c++ python3-numpy python3-numpy-devel python3-scipy python3-matplotlib python3-IPython python3-Jinja2 libxml2-devel lzlib-devel 
 
 mkdir orange3env
 virtualenv -p python3 --system-site-packages orange3env
@@ -13,4 +13,4 @@ pip install -r requirements.txt
 python setup.py develop
 pip install Orange-Bioinformatics networkx python-igraph
 
-ipython notebook
+ipython3 notebook
